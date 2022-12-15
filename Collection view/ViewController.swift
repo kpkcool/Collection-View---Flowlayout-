c9ef8ba349bb7905540ecc710a3e7452b69fafa6
K Praveen Kumar <kpkcool@gmail.com>
2022-12-15 15:56:20 +0530

    Initial Commit

---
+++ Collection view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection view
+//
+//  Created by K Praveen Kumar on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
